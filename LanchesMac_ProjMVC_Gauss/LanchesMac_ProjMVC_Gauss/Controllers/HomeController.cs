b3fcdc471af2d6fc7330f75fb4b6317ddf27a4b9
zarkio42 <pedro.contato24@gmail.com>
2023-08-30 19:27:28 -0300

    new project

---
+++ LanchesMac_ProjMVC_Gauss/LanchesMac_ProjMVC_Gauss/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac_ProjMVC_Gauss.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanchesMac_ProjMVC_Gauss.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
