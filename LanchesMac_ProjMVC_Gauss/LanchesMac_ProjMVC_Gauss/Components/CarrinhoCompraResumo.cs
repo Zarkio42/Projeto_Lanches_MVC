d2c6f8c4a897948eafef42f031f2c7a28411ec57
Pedro Cardoso <pedro.contato24@gmail.com>
2023-10-04 01:14:37 -0300

    update viewComponent carrinho

---
+++ LanchesMac_ProjMVC_Gauss/LanchesMac_ProjMVC_Gauss/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,35 @@
+﻿using LanchesMac_ProjMVC_Gauss.Models;
+using LanchesMac_ProjMVC_Gauss.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac_ProjMVC_Gauss.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(),
+            //    new CarrinhoCompraItem()
+            //};
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+
+            };
+            return View(carrinhoCompraVM);
+        }
+    }
+}
