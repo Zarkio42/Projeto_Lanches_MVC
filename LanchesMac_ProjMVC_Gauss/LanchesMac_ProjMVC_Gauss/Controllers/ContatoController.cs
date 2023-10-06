0309d9054dc9fcdc1aa2d1b39ec2708a775b2349
Pedro Cardoso <pedro.contato24@gmail.com>
2023-10-06 19:27:18 -0300

    taghelper

---
+++ LanchesMac_ProjMVC_Gauss/LanchesMac_ProjMVC_Gauss/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac_ProjMVC_Gauss.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LanchesMac_ProjMVC_Gauss/LanchesMac_ProjMVC_Gauss/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LanchesMac_ProjMVC_Gauss.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
